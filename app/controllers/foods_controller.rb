08747f1472f0866ba29bb65f99353d04e9d6c948
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-18 11:44:26 +0300

    Update foods_controller.rb

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,21 @@
 class FoodsController < ApplicationController
   def index
+    @foods = Food.all
+    @food = Food.new
   end
 
   def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :quantity)
   end
 end
