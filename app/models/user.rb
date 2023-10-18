a6cf073a778aac3dd70c9672db0627f4411ada40
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-18 13:33:42 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ApplicationRecord
-  # dont accept user with the same name, no matter it is capitalized or not.
-  validates :name, uniqueness: { case_sensitive: false }
-
-  has_many :foods, foreign_key: :buyer_id
-  has_many :recipes, foreign_key: :author_id
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :foods, dependent: :destroy
+  has_many :recipes, dependent: :destroy
+
+  validates :name, presence: true
 end
