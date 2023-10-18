80b2fb30ab7064d8e12f9c7e5700405a99c9190a
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-18 13:55:10 +0300

    fix linters

--- spec/controllers/foods_controller_spec.rb
+++ spec/controllers/foods_controller_spec.rb
@@ -1,69 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe FoodsController, type: :controller do
-  describe "#index" do
-    it "assigns @foods" do
+  describe '#index' do
+    it 'assigns @foods' do
       food1 = create(:food)
       food2 = create(:food)
       get :index
       expect(assigns(:foods)).to eq([food1, food2])
     end
 
-    it "renders the index template" do
+    it 'renders the index template' do
       get :index
       expect(response).to render_template(:index)
     end
   end
 
-  describe "#new" do
-    it "assigns a new food to @food" do
+  describe '#new' do
+    it 'assigns a new food to @food' do
       get :new
       expect(assigns(:food)).to be_a_new(Food)
     end
 
-    it "renders the new template" do
+    it 'renders the new template' do
       get :new
       expect(response).to render_template(:new)
     end
   end
 
-  describe "#create" do
-    context "with valid parameters" do
-      it "creates a new food" do
-        expect {
+  describe '#create' do
+    context 'with valid parameters' do
+      it 'creates a new food' do
+        expect do
           post :create, params: { food: attributes_for(:food) }
-        }.to change(Food, :count).by(1)
+        end.to change(Food, :count).by(1)
       end
 
-      it "redirects to the index page" do
+      it 'redirects to the index page' do
         post :create, params: { food: attributes_for(:food) }
         expect(response).to redirect_to(foods_path)
       end
     end
 
-    context "with invalid parameters" do
-      it "does not create a new food" do
-        expect {
+    context 'with invalid parameters' do
+      it 'does not create a new food' do
+        expect do
           post :create, params: { food: attributes_for(:food, name: nil) }
-        }.not_to change(Food, :count)
+        end.not_to change(Food, :count)
       end
 
-      it "redirects to the new page" do
+      it 'redirects to the new page' do
         post :create, params: { food: attributes_for(:food, name: nil) }
         expect(response).to redirect_to(new_food_path)
       end
     end
   end
 
-  describe "#destroy" do
-    it "destroys the food" do
+  describe '#destroy' do
+    it 'destroys the food' do
       food = create(:food)
-      expect {
+      expect do
         delete :destroy, params: { id: food.id }
-      }.to change(Food, :count).by(-1)
+      end.to change(Food, :count).by(-1)
     end
 
-    it "redirects to the index page" do
+    it 'redirects to the index page' do
       food = create(:food)
       delete :destroy, params: { id: food.id }
       expect(response).to redirect_to(foods_path)
